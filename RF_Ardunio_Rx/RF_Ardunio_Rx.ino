7c1cc175cb63d3c4dbfe1d561ce233d78dfe951a
Himanshu Sharma <himanshus2847@gmail.com>
2019-11-26 22:41:52 +0530

    Add files via upload

---
+++ RF_Ardunio_Rx/RF_Ardunio_Rx.ino
@@ -0,0 +1,28 @@
+#include <VirtualWire.h>
+const int ledPin = 13;
+const int datain = 11;
+void setup()
+{
+    vw_set_ptt_inverted(true);
+    vw_set_rx_pin(datain);
+    vw_setup(2000);
+    pinMode(ledPin, OUTPUT);
+    vw_rx_start();
+}
+    void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN; 
+ 
+    if (vw_get_message(buf, &buflen))
+    {
+      if(buf[0]==102)
+      {  
+       digitalWrite(ledPin,HIGH);
+      }  
+      if(buf[0]==98)
+      {
+       digitalWrite(ledPin,LOW);
+      }
+    }
+}
---
+++ RF_Ardunio_Tx/RF_Ardunio_Tx.ino
@@ -0,0 +1,24 @@
+#include <VirtualWire.h>
+const int ledPin = 13;
+char *data;
+void setup() 
+{
+  pinMode(ledPin,OUTPUT);
+  vw_set_ptt_inverted(true);
+  vw_set_tx_pin(12);
+  vw_setup(4000);
+}
+
+void loop()
+{
+  data="f";
+  vw_send((uint8_t *)data, strlen(data));
+  vw_wait_tx();
+  digitalWrite(ledPin,HIGH);
+  delay(2000);
+  data="b";
+  vw_send((uint8_t *)data, strlen(data));
+  vw_wait_tx();
+  digitalWrite(ledPin,LOW);
+  delay(2000);
+}
