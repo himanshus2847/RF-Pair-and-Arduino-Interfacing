fc83174d18d1ab746b227d1271af252cb4709ef6
Himanshu Sharma <himanshus2847@gmail.com>
2020-03-31 14:35:15 +0530

    Update RF_Ardunio_Rx.ino

--- RF_Ardunio_Rx/RF_Ardunio_Rx.ino
+++ RF_Ardunio_Rx/RF_Ardunio_Rx.ino
@@ -1,26 +1,26 @@
 #include <VirtualWire.h>
-const int ledPin = 13;
-const int datain = 11;
+#define ledPin 6
+
 void setup()
 {
-    vw_set_ptt_inverted(true);
-    vw_set_rx_pin(datain);
+    vw_set_rx_pin(11);
     vw_setup(2000);
     pinMode(ledPin, OUTPUT);
     vw_rx_start();
 }
-    void loop()
+
+void loop()
 {
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN; 
  
     if (vw_get_message(buf, &buflen))
     {
-      if(buf[0]==102)
+      if(buf[0]=='a')
       {  
        digitalWrite(ledPin,HIGH);
       }  
-      if(buf[0]==98)
+      else if(buf[0]=='b')
       {
        digitalWrite(ledPin,LOW);
       }
