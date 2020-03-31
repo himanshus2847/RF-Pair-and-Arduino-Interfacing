22d927c04b2a481b8414df252bb2573c68f98ce7
Himanshu Sharma <himanshus2847@gmail.com>
2020-03-31 14:35:48 +0530

    Update RF_Ardunio_Tx.ino

--- RF_Ardunio_Tx/RF_Ardunio_Tx.ino
+++ RF_Ardunio_Tx/RF_Ardunio_Tx.ino
@@ -1,24 +1,36 @@
 #include <VirtualWire.h>
-const int ledPin = 13;
+
+#define button 6
+
 char *data;
+int val;
+int value = 0;
+
 void setup() 
 {
-  pinMode(ledPin,OUTPUT);
-  vw_set_ptt_inverted(true);
   vw_set_tx_pin(12);
-  vw_setup(4000);
+  vw_setup(2000);
+  pinMode(button, INPUT_PULLUP);
 }
 
 void loop()
 {
-  data="f";
-  vw_send((uint8_t *)data, strlen(data));
-  vw_wait_tx();
-  digitalWrite(ledPin,HIGH);
-  delay(2000);
-  data="b";
-  vw_send((uint8_t *)data, strlen(data));
-  vw_wait_tx();
-  digitalWrite(ledPin,LOW);
-  delay(2000);
+  val = digitalRead(button);
+  if(val == LOW && value == 0)
+  {
+    data="a";
+    vw_send((uint8_t *)data, strlen(data));
+    vw_wait_tx();
+    delay(500);
+    value = 1;
+  }
+  else if(val == LOW && value == 1)
+  {
+    data="b";
+    vw_send((uint8_t *)data, strlen(data));
+    vw_wait_tx();
+    delay(500);
+    value = 0;
+  }
+  delay(200);
 }
